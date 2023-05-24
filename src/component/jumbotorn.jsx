ab6fce44b232426603d680a003d843270d0a54c5
Natalia Matondang <nataliamtd.21@gmail.com>
2023-05-25 03:22:47 +0700

    menyelesaikan landing page

--- src/component/jumbotorn.jsx
+++ src/component/jumbotorn.jsx
@@ -1,16 +1,55 @@
 import Image1 from "../assets/image/Saly-3.png"
-function Jumbotorn() {
+import Image2 from "../assets/image/Image2.png"
+import Image3 from "../assets/image/Image3.png"
+import "../assets/landing-page.css"
+function Jumbotron1() {
     return (
-        <section className="Jumbotron">
-            <img className="gambar_jumbotorn" src={Image1}/>
+        <section className="Jumbotron1">
+            <img className="gambar_jumbotron" src={Image1}/>
             
-            <div className="description">
-            <h2>Laboratorium Informatika</h2>
-            <h2 id="colored">UPN "Veteran" Jawa Timur</h2>
-            <p>Laboratorium Sistem Cerdas dan Robotika (Lab SCR) atau Intelligent Systems And Robotic Laboratory (ISR Lab) merupakan salah satu Laboratorium di bawah pengelolaan Program Studi Informatika Fakultas Ilmu Komputer UPN Veteran Jawa Timur yang fokus pada keilmuan berkaitan dengan Sistem Cerdas dan Robotika.</p>
+            <div className="description"> 
+            <h2>Menjaga Lingkungan Bukan Angan-angan, tapi Tindakan</h2>
+            <p>Mari bergabung bersama Trash Hunter agar dapat menemukan beragam kegiatan peduli lingkungan di seluruh Indonesia dan mengambil bagian dalam gerakan menuju bumi yang lebih bersih.</p>
             </div>
         </section>
     );
-  }
-  
-  export default Jumbotorn;
+}
+
+
+function Jumbotron2() {
+    return (
+        <section className="Jumbotron2">
+            <img className="gambar_jumbotron" src={Image2}/>
+            
+            <div className="description2"> 
+                <p>Trash Hunter menjadi media untuk mempromosikan kegiatan peduli lingkungan yang akan diselenggarakan.</p>
+                <p>Menemukan dan mendaftar menjadi bagian dari kegiatan peduli lingkungan yang ada di sekitar anda.</p>
+                <p>Bertemu dengan teman yang memiliki visi dan misi terhadap keberlangsungan bumi.</p>
+            </div>
+        </section>
+    );
+}
+
+function Jumbotron3() {
+    return (
+        <section className="Jumbotron3">
+            <div className="description3"> 
+                <p>Kerusakan alam bukan terjadi pada kita, tapi karena kita.</p>
+            </div>
+        </section>
+    );
+}
+
+function Jumbotron4() {
+    return (
+        <section className="Jumbotron4">
+            <img className="gambar_jumbotron" src={Image3}/>
+            
+            <div className="description4">
+            <p>Mari bergabung dengan Trash Hunter</p>
+            <h2>Merawat lingkungan hari ini untuk kehidupan yang lebih baik besok.</h2>
+            </div>
+        </section>
+    );
+}
+export {Jumbotron1, Jumbotron2, Jumbotron3, Jumbotron4};
