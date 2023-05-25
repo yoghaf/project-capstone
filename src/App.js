1a13fb26680be26dd48192bde3935abf6ac7f831
yoghaf <utomoyogafirmansyah@gmail.com>
2023-05-25 19:20:07 +0700

    update jumbotron

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import "./App.css";
 import React from "react";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
 import CollapsibleExample from "./component/navbar-landing";
-import {Jumbotron1, Jumbotron2, Jumbotron3, Jumbotron4} from "./component/jumbotorn";
+import { Jumbotron1, Jumbotron2, Jumbotron3, Jumbotron4 } from "./component/jumbotorn";
 
 function App() {
   return (
     <div className="App">
-      <CollapsibleExample></CollapsibleExample>
-      <Jumbotron1></Jumbotron1>
-      <Jumbotron2></Jumbotron2>
-      <Jumbotron3></Jumbotron3>
-      <Jumbotron4></Jumbotron4>
+      <CollapsibleExample />
+      <Jumbotron1 />
+      <Jumbotron2 />
+      <Jumbotron3 />
+      <Jumbotron4 />
     </div>
   );
 }
--- src/component/jumbotorn.jsx
+++ src/component/jumbotorn.jsx
@@ -1,55 +1,54 @@
-import Image1 from "../assets/image/Saly-3.png"
-import Image2 from "../assets/image/Image2.png"
-import Image3 from "../assets/image/Image3.png"
-import "../assets/landing-page.css"
+import Image1 from "../assets/image/Saly-3.png";
+import Image2 from "../assets/image/Image2.png";
+import Image3 from "../assets/image/Image3.png";
+import "../assets/landing-page.css";
 function Jumbotron1() {
-    return (
-        <section className="Jumbotron1">
-            <img className="gambar_jumbotron" src={Image1}/>
-            
-            <div className="description"> 
-            <h2>Menjaga Lingkungan Bukan Angan-angan, tapi Tindakan</h2>
-            <p>Mari bergabung bersama Trash Hunter agar dapat menemukan beragam kegiatan peduli lingkungan di seluruh Indonesia dan mengambil bagian dalam gerakan menuju bumi yang lebih bersih.</p>
-            </div>
-        </section>
-    );
+  return (
+    <section className="Jumbotron1">
+      <img className="gambar_jumbotron" src={Image1} alt="" />
+
+      <div className="description">
+        <h2>Menjaga Lingkungan Bukan Angan-angan, tapi Tindakan</h2>
+        <p>Mari bergabung bersama Trash Hunter agar dapat menemukan beragam kegiatan peduli lingkungan di seluruh Indonesia dan mengambil bagian dalam gerakan menuju bumi yang lebih bersih.</p>
+      </div>
+    </section>
+  );
 }
 
-
 function Jumbotron2() {
-    return (
-        <section className="Jumbotron2">
-            <img className="gambar_jumbotron" src={Image2}/>
-            
-            <div className="description2"> 
-                <p>Trash Hunter menjadi media untuk mempromosikan kegiatan peduli lingkungan yang akan diselenggarakan.</p>
-                <p>Menemukan dan mendaftar menjadi bagian dari kegiatan peduli lingkungan yang ada di sekitar anda.</p>
-                <p>Bertemu dengan teman yang memiliki visi dan misi terhadap keberlangsungan bumi.</p>
-            </div>
-        </section>
-    );
+  return (
+    <section className="Jumbotron2">
+      <img className="gambar_jumbotron" src={Image2} alt="" />
+
+      <div className="description2">
+        <p>Trash Hunter menjadi media untuk mempromosikan kegiatan peduli lingkungan yang akan diselenggarakan.</p>
+        <p>Menemukan dan mendaftar menjadi bagian dari kegiatan peduli lingkungan yang ada di sekitar anda.</p>
+        <p>Bertemu dengan teman yang memiliki visi dan misi terhadap keberlangsungan bumi.</p>
+      </div>
+    </section>
+  );
 }
 
 function Jumbotron3() {
-    return (
-        <section className="Jumbotron3">
-            <div className="description3"> 
-                <p>Kerusakan alam bukan terjadi pada kita, tapi karena kita.</p>
-            </div>
-        </section>
-    );
+  return (
+    <section className="Jumbotron3">
+      <div className="description3">
+        <p>Kerusakan alam bukan terjadi pada kita, tapi karena kita.</p>
+      </div>
+    </section>
+  );
 }
 
 function Jumbotron4() {
-    return (
-        <section className="Jumbotron4">
-            <img className="gambar_jumbotron" src={Image3}/>
-            
-            <div className="description4">
-            <p>Mari bergabung dengan Trash Hunter</p>
-            <h2>Merawat lingkungan hari ini untuk kehidupan yang lebih baik besok.</h2>
-            </div>
-        </section>
-    );
+  return (
+    <section className="Jumbotron4">
+      <img className="gambar_jumbotron" src={Image3} alt="" />
+
+      <div className="description4">
+        <p>Mari bergabung dengan Trash Hunter</p>
+        <h2>Merawat lingkungan hari ini untuk kehidupan yang lebih baik besok.</h2>
+      </div>
+    </section>
+  );
 }
-export {Jumbotron1, Jumbotron2, Jumbotron3, Jumbotron4};
+export { Jumbotron1, Jumbotron2, Jumbotron3, Jumbotron4 };
