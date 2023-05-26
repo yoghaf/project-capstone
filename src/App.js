489dd3454ce465255313c5d41c5b713d8f1381ed
yoghaf <utomoyogafirmansyah@gmail.com>
2023-05-26 12:20:02 +0700

    fix routing in dashboard

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ import LandingPage from "./pages/LandingPage";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import Dashboard from "./pages/dashboard/Dashboard";
-import DashboardRegistered from "./pages/dashboard/DashboardRegistered";
+import RegisteredEvent from "./parts/dashboard/RegisteredEvent";
+import Event from "./parts/dashboard/Event";
 
 function App() {
   return (
@@ -14,8 +15,12 @@ function App() {
       <Route path="/" element={<LandingPage />} />
       <Route path="/login" element={<Login />} />
       <Route path="/signup" element={<Register />} />
-      <Route path="/dashboard" element={<Dashboard />} />
-      <Route path="/dashboard/registeredevent" element={<DashboardRegistered />} />
+      <Route path="/dashboard/" element={<Dashboard />}>
+        <Route index element={<Event />} />
+        <Route path="registeredevent" element={<RegisteredEvent />} />
+        <Route path=":id/myevent" element={<Dashboard />} />
+        <Route path="save" element={<Dashboard />} />
+      </Route>
     </Routes>
   );
 }
--- src/components/dashboard/Nav.jsx
+++ src/components/dashboard/Nav.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 import "../../assets/style/Nav/nav.css";
 import { buttonOpen, buttonClose, onSticky } from "../../assets/style/Nav/navAnimation";
 
-function Navigation({ handleRegisteredEventClick }) {
+function Navigation() {
   const [isNavOpen, setIsNavOpen] = useState(false);
 
   const toggleNav = () => {
