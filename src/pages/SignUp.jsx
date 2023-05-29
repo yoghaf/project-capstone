a267d9688a5293681a705c8da4c062c14a04ccc3
Natalia Matondang <nataliamtd.21@gmail.com>
2023-05-29 18:33:59 +0700

    menyatukan halaman register dengan dashboard

---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "../assets/style/signup.css"
+
+function SignUp() {
+  return (
+    <>
+      <div className="Title">
+      <h1>Trash Hunter</h1>
+    </div>
+    <div className="SpaceLayout"></div>
+    <div className="Layout0">
+      <img src="./images/img-register.png" className="ImageSmall" alt=""></img>
+      <div className="FormLayout">
+
+        <form className="Form" action="register.php" method="POST">
+          <fieldset>
+          <legend>Create your account</legend>
+          <p className="TextField">
+              <input className="InputText" type="text" name="username" placeholder="username" />
+          </p>
+          <p className="TextField">
+              <input className="InputText" type="email" name="email" placeholder="email" />
+          </p>
+          <p className="TextField">
+              <input className="InputText" type="password" name="password" placeholder="password" />
+          </p>
+          <button>
+              <input className="InputButton" type="submit" name="submit" value="SIGN UP" />
+          </button>
+          <p className="TextQuestion">Already have an account? <Link to={"/login"}>Log in</Link></p>
+          </fieldset>
+        </form>
+
+      </div>
+      <img src="./images/img-register.png" className="ImageLarge" alt=""></img>
+    </div>
+    </>
+  );
+}
+
+export default SignUp;
