e2e5c807d55e88217532af32f13db9f3407d77d3
yoghaf <utomoyogafirmansyah@gmail.com>
2023-06-08 11:23:33 +0700

    fix myevent for netlify

--- src/parts/dashboard/DetailMyEvent.jsx
+++ src/parts/dashboard/DetailMyEvent.jsx
@@ -1,43 +1,40 @@
 import React, { useEffect, useState } from "react";
-import { Link, useParams, useNavigate } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 import Table from "react-bootstrap/Table";
 import "../../assets/style/detail.css";
 import "../../assets/style/myevent.css";
 import supabase from "../../config/supabaseClient";
 
 function DetailMyEvent() {
-    const [fetchError, setFetchError] = useState(null);
+  const [fetchError, setFetchError] = useState(null);
 
-    const [event, setEvent] = useState(null);
-    const [participants, setParticipants] = useState(null);
-    const { id } = useParams();
-    useEffect(() => {
-      const fetchEvent = async () => {
-        const { data, error } = await supabase.from("event").select().eq("id", id);
-  
-        if (error) {
-          setFetchError("Could not fetch the events");
-          setEvent(null);
-          console.log(error);
-        }
-        if (data) {
-          setEvent(data);
-  
-          setFetchError(null);
-        }
+  const [event, setEvent] = useState(null);
+  const [participants, setParticipants] = useState(null);
+  const { id } = useParams();
+  useEffect(() => {
+    const fetchEvent = async () => {
+      const { data, error } = await supabase.from("event").select().eq("id", id);
 
-        const  peserta  = await supabase.from("daftar").select().eq("id-event", id);
-          if (peserta) {
-            setParticipants(peserta.data);
-            setFetchError(null);
-          }
-      };
-  
-      fetchEvent();
-    }, [id]);
+      if (error) {
+        setFetchError("Could not fetch the events");
+        setEvent(null);
+        console.log(error);
+      }
+      if (data) {
+        setEvent(data);
 
-    
+        setFetchError(null);
+      }
 
+      const peserta = await supabase.from("daftar").select().eq("id-event", id);
+      if (peserta) {
+        setParticipants(peserta.data);
+        setFetchError(null);
+      }
+    };
+
+    fetchEvent();
+  }, [id]);
 
   return (
     <>
@@ -60,7 +57,9 @@ function DetailMyEvent() {
                   <p className="text date">Date Event {event[0]["event-start"]}</p>
                 </div>
                 <div className="location-image">
-                  <p className="text date">{event[0]["location"]}, Kota {event[0]["city"]}</p>
+                  <p className="text date">
+                    {event[0]["location"]}, Kota {event[0]["city"]}
+                  </p>
                 </div>
               </div>
               <div className="row" style={{ padding: "25px 68px" }}>
@@ -87,8 +86,12 @@ function DetailMyEvent() {
                   </ol>
                 </div>
                 <div className="row" style={{ paddingTop: "10px" }}>
-                    <p className="text" id="textLink">Link Media Social</p>
-                    <p className="text" id="link">{event[0]["link-invite"]}</p>  
+                  <p className="text" id="textLink">
+                    Link Media Social
+                  </p>
+                  <p className="text" id="link">
+                    {event[0]["link-invite"]}
+                  </p>
                 </div>
               </div>
             </div>
@@ -97,34 +100,35 @@ function DetailMyEvent() {
       </div>
 
       <h1 className="event-title">Daftar Peserta</h1>
-      
+
       <center>
-      <div className="container-table">
-        <Table bordered hover style={{fontFamily: 'Bold'}}>
-          <tbody>
-            <tr className="header" style={{fontFamily: 'Bold'}}>
-              <th>NAMA</th>
-              <th>EMAIL</th>
-              <th>NO.TELP</th>
-              <th>DOMISILI</th>
-            </tr>
-            {
-            !participants?<p>Belum ada peserta yang mendaftar</p>
-                :participants.map((participant, i) => {
-              return (
-                <tr>
-                  <td align="left">{participant.name}</td>
-                  <td>{participant.email}</td>
-                  <td>{participant.notelp}</td>
-                  <td>{participant.domicile}</td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </Table>
-      </div>
-    </center>
-      
+        <div className="container-table">
+          <Table bordered hover style={{ fontFamily: "Bold" }}>
+            <tbody>
+              <tr className="header" style={{ fontFamily: "Bold" }}>
+                <th>NAMA</th>
+                <th>EMAIL</th>
+                <th>NO.TELP</th>
+                <th>DOMISILI</th>
+              </tr>
+              {!participants ? (
+                <p>Belum ada peserta yang mendaftar</p>
+              ) : (
+                participants.map((participant, i) => {
+                  return (
+                    <tr>
+                      <td align="left">{participant.name}</td>
+                      <td>{participant.email}</td>
+                      <td>{participant.notelp}</td>
+                      <td>{participant.domicile}</td>
+                    </tr>
+                  );
+                })
+              )}
+            </tbody>
+          </Table>
+        </div>
+      </center>
     </>
   );
 }
--- src/parts/dashboard/MyEvent.jsx
+++ src/parts/dashboard/MyEvent.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { Card } from "react-bootstrap";
-import Alert from "react-bootstrap/Alert";
+
 import Button from "react-bootstrap/Button";
 import Col from "react-bootstrap/Col";
 import Form from "react-bootstrap/Form";
@@ -12,7 +12,7 @@ import { BsPlus } from "react-icons/bs";
 import { CiSearch } from "react-icons/ci";
 import { LuTrash } from "react-icons/lu";
 import "../../assets/style/myevent.css";
-import '../../assets/fonts/Nunito-Bold.ttf'
+import "../../assets/fonts/Nunito-Bold.ttf";
 
 function MyEvent() {
   const [modalShow, setModalShow] = useState(false);
@@ -34,7 +34,7 @@ function MyEvent() {
   const [description, setDescription] = useState("");
   // state city
   const [city, setCity] = useState("");
-  const [cities, setCities] = useState([
+  const cities = [
     {
       id: 1,
       title: "Bogor",
@@ -47,7 +47,21 @@ function MyEvent() {
       id: 3,
       title: "Bandung",
     },
-  ]);
+  ];
+  // const [cities, setCities] = useState([
+  //   {
+  //     id: 1,
+  //     title: "Bogor",
+  //   },
+  //   {
+  //     id: 2,
+  //     title: "Jakarta",
+  //   },
+  //   {
+  //     id: 3,
+  //     title: "Bandung",
+  //   },
+  // ]);
   // state location
   const [location, setLocation] = useState("");
   // state link group
@@ -109,36 +123,25 @@ function MyEvent() {
   return (
     <div>
       <Row className="seacrhAdd mt-4">
-        <Col  className="me-auto ms-0" xs={10} md={10}>
+        <Col className="me-auto ms-0" xs={10} md={10}>
           <div className="card-search">
             <Form className="search-box">
               <CiSearch className="search-icon" />
-              <Form.Control
-                type="search"
-                icon="fa-search"
-                placeholder="Search Event"
-                aria-label="Search"
-                className="search-input"
-              />
+              <Form.Control type="search" icon="fa-search" placeholder="Search Event" aria-label="Search" className="search-input" />
             </Form>
           </div>
         </Col>
         <Col className="ms-auto me-0" xs={1} md={1}>
-          <Button
-            className="buttonAdd"
-            size="sm"
-            variant="success"
-            onClick={() => setShowModalAdd(true)}
-          >
+          <Button className="buttonAdd" size="sm" variant="success" onClick={() => setShowModalAdd(true)}>
             <BsPlus />
           </Button>
         </Col>
       </Row>
       <center>
         <div className="container-table">
-          <Table bordered hover style={{fontFamily: 'Bold'}}>
+          <Table bordered hover style={{ fontFamily: "Bold" }}>
             <tbody>
-              <tr className="header" style={{fontFamily: 'Bold'}}>
+              <tr className="header" style={{ fontFamily: "Bold" }}>
                 <th>KEGIATAN</th>
                 <th>PESERTA</th>
                 <th>STATUS</th>
@@ -146,22 +149,35 @@ function MyEvent() {
               </tr>
               {data.map((item, i) => {
                 return (
-                  <tr>
-                    <td align="left"><Link  className="text-black" to={"/dashboard/myevent/"+item.id}>{item.activity}</Link></td>
+                  <tr key={i}>
+                    <td align="left">
+                      <Link className="text-black" to={"/dashboard/myevent/" + item.id}>
+                        {item.activity}
+                      </Link>
+                    </td>
                     <td>{item?.audience}</td>
-                    {item?.status == "live" ? (
-                      <td style={{
-                        color: '#52AD32',
-                        fontFamily: 'Bold'
-                      }}>BERLANGSUNG</td>
+                    {item?.status === "live" ? (
+                      <td
+                        style={{
+                          color: "#52AD32",
+                          fontFamily: "Bold",
+                        }}
+                      >
+                        BERLANGSUNG
+                      </td>
                     ) : (
-                      <td style={{
-                        color: '#AD3232'
-                      }}>SELESAI</td>
+                      <td
+                        style={{
+                          color: "#AD3232",
+                        }}
+                      >
+                        SELESAI
+                      </td>
                     )}
                     <td>
-                      <Button className="deleteButton"
-                        style={{fontFamily: 'Bold'}}
+                      <Button
+                        className="deleteButton"
+                        style={{ fontFamily: "Bold" }}
                         variant="danger"
                         onClick={() => {
                           setHandleId(i);
@@ -180,26 +196,15 @@ function MyEvent() {
       </center>
 
       {/* modal delete */}
-      <Modal
-        show={modalShow}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-      >
-        <Modal.Header
-          style={{ background: "#F45252" }}
-          closeButton
-          onClick={() => setModalShow(false)}
-        >
+      <Modal show={modalShow} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
+        <Modal.Header style={{ background: "#F45252" }} closeButton onClick={() => setModalShow(false)}>
           <Modal.Title id="contained-modal-title-vcenter"></Modal.Title>
         </Modal.Header>
         <Modal.Body style={{ background: "#F45252" }}>
-          <h5 style={{ textAlign: "center", color: "white", fontFamily: 'Bold' }}>
-            Apakah anda yakin ingin menghapus event ini ?
-          </h5>
+          <h5 style={{ textAlign: "center", color: "white", fontFamily: "Bold" }}>Apakah anda yakin ingin menghapus event ini ?</h5>
           <div
             style={{
-              fontFamily: 'Bold',
+              fontFamily: "Bold",
               justifyContent: "center",
               display: "flex",
               alignItems: "center",
@@ -209,9 +214,9 @@ function MyEvent() {
               variant="light"
               size="sm"
               onClick={() => {
-                let newData = [...data]
-                newData.splice(handleId, 1)
-                setData(newData)
+                let newData = [...data];
+                newData.splice(handleId, 1);
+                setData(newData);
                 setModalShow(false);
                 setHandleId(null);
               }}
@@ -223,126 +228,73 @@ function MyEvent() {
       </Modal>
 
       {/* modal add */}
-      <Modal
-        show={showModalAdd}
-        size="lg"
-        aria-labelledby="contained-modal-title-vcenter"
-        centered
-      >
+      <Modal show={showModalAdd} size="lg" aria-labelledby="contained-modal-title-vcenter" centered>
         <Modal.Header closeButton onClick={() => setShowModalAdd(false)}>
           <Modal.Title id="contained-modal-title-vcenter"></Modal.Title>
         </Modal.Header>
         <Modal.Body>
           {/* form add event */}
           <div>
-            <p className="title-addevent" style={{fontFamily: 'Bold'}}>ADD NEW EVENT</p>
-            <Form style={{fontFamily: 'Bold'}}>
+            <p className="title-addevent" style={{ fontFamily: "Bold" }}>
+              ADD NEW EVENT
+            </p>
+            <Form style={{ fontFamily: "Bold" }}>
               {/* name event */}
               <Form.Group className="d-flex align-items-center mb-2 mt-2">
                 <Form.Label className="w-25">Name of Event</Form.Label>
-                <Form.Control
-                  className="w-100"
-                  placeholder=""
-                  value={nameEvent}
-                  onChange={(v) => setNameEvent(v.target.value)}
-                />
+                <Form.Control className="w-100" placeholder="" value={nameEvent} onChange={(v) => setNameEvent(v.target.value)} />
               </Form.Group>
               {/* registration */}
               <Form.Group className="d-flex align-items-center mb-2 mt-2">
                 <Form.Label className="w-25">Regitration</Form.Label>
                 <div className="d-flex align-items-center flex-grow-1">
-                  <Form.Control
-                    className="formDate w-45"
-                    type="date"
-                    value={dateRegisStart == null ? new Date() : dateRegisStart}
-                    onChange={(v) => setDateRegisStart(v.target.value)}
-                  />
+                  <Form.Control className="formDate w-45" type="date" value={dateRegisStart == null ? new Date() : dateRegisStart} onChange={(v) => setDateRegisStart(v.target.value)} />
                   <p style={{ marginLeft: 16, marginRight: 16 }}>-</p>
-                  <Form.Control
-                    className="formDate w-45"
-                    type="date"
-                    value={dateRegisEnd == null ? new Date() : dateRegisEnd}
-                    onChange={(v) => setDateRegisEnd(v.target.value)}
-                  />
+                  <Form.Control className="formDate w-45" type="date" value={dateRegisEnd == null ? new Date() : dateRegisEnd} onChange={(v) => setDateRegisEnd(v.target.value)} />
                 </div>
               </Form.Group>
               {/* date */}
               <Form.Group className="d-flex align-items-center mb-2 mt-2">
                 <Form.Label className="w-25">Date</Form.Label>
                 <div className="d-flex align-items-center flex-grow-1">
-                  <Form.Control
-                    className="formDate w-45"
-                    type="date"
-                    value={startDate}
-                    onChange={(v) => setStartDate(v.target.value)}
-                  />
+                  <Form.Control className="formDate w-45" type="date" value={startDate} onChange={(v) => setStartDate(v.target.value)} />
                   <p style={{ marginLeft: 16, marginRight: 16 }}>-</p>
-                  <Form.Control
-                    className="formDate w-45"
-                    type="date"
-                    value={endData}
-                    onChange={(v) => setEndDate(v.target.value)}
-                  />
+                  <Form.Control className="formDate w-45" type="date" value={endData} onChange={(v) => setEndDate(v.target.value)} />
                 </div>
               </Form.Group>
               {/* link image */}
               <Form.Group className="d-flex align-items-center mb-2 mt-2">
                 <Form.Label className="w-25">Link image</Form.Label>
-                <Form.Control
-                  className="w-100"
-                  placeholder=""
-                  value={link}
-                  onChange={(v) => setLink(v.target.value)}
-                />
+                <Form.Control className="w-100" placeholder="" value={link} onChange={(v) => setLink(v.target.value)} />
               </Form.Group>
               {/* description */}
               <Form.Group className="d-flex mb-2 mt-2">
                 <Form.Label className="w-25">Description</Form.Label>
-                <Form.Control
-                  className="w-100"
-                  placeholder=""
-                  as="textarea"
-                  rows={3}
-                  value={description}
-                  onChange={(v) => setDescription(v.target.value)}
-                />
+                <Form.Control className="w-100" placeholder="" as="textarea" rows={3} value={description} onChange={(v) => setDescription(v.target.value)} />
               </Form.Group>
               {/* city */}
               <Form.Group className="d-flex mb-2 mt-2">
                 <Form.Label className="w-25">City</Form.Label>
-                <Form.Select
-                  className="w-100"
-                  placeholder=""
-                  value={city}
-                  onChange={(v) => setCity(v.target.value)}
-                >
+                <Form.Select className="w-100" placeholder="" value={city} onChange={(v) => setCity(v.target.value)}>
                   <option>Select city</option>
                   {cities.map((item, i) => {
-                    return <option value={item?.title}>{item?.title}</option>;
+                    return (
+                      <option key={i} value={item?.title}>
+                        {item?.title}
+                      </option>
+                    );
                   })}
                 </Form.Select>
               </Form.Group>
               {/* location */}
               <Form.Group className="d-flex mb-2 mt-2">
                 <Form.Label className="w-25">Location</Form.Label>
-                <Form.Control
-                  className="w-100"
-                  placeholder=""
-                  as="textarea"
-                  rows={3}
-                  value={location}
-                  onChange={(v) => setLocation(v.target.value)}
-                />
+                <Form.Control className="w-100" placeholder="" as="textarea" rows={3} value={location} onChange={(v) => setLocation(v.target.value)} />
               </Form.Group>
               {/* Link invite Group */}
               <Form.Group className="d-flex align-items-center mb-2 mt-2">
                 <Form.Label className="w-25">Link invite Group</Form.Label>
-                <Form.Control
-                  className="w-100"
-                  placeholder=""
-                  value={linkGroup}
-                  onChange={(v) => setLinkGroup(v.target.value)}
-                />
+                <Form.Control className="w-100" placeholder="" value={linkGroup} onChange={(v) => setLinkGroup(v.target.value)} />
               </Form.Group>
               {/* button add new image */}
               <Button variant="secondary" className="w-100 mb-4 mt-2">
@@ -363,11 +315,7 @@ function MyEvent() {
                 </Card>
               </div>
               {/* button save */}
-              <Button
-                variant="success"
-                className="w-100 mb-4 mt-2"
-                onClick={() => handleSaveEvent()}
-              >
+              <Button variant="success" className="w-100 mb-4 mt-2" onClick={() => handleSaveEvent()}>
                 SIMPAN
               </Button>{" "}
             </Form>
