76e9aaf88c4769560ba9fae2d31d2e336c42c373
yoghaf <utomoyogafirmansyah@gmail.com>
2023-06-10 11:06:35 +0700

    update daftar to event

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,93 +1,96 @@
 import React from "react";
 import { Link, useNavigate } from "react-router-dom";
-import { useEffect,useState } from 'react';
-import "../assets/style/signup.css"
+import { useEffect, useState } from "react";
+import "../assets/style/signup.css";
 import supabase from "../config/supabaseClient";
 
-function Login({setToken}) {
-  let navigate = useNavigate()
+function Login({ setToken }) {
+  let navigate = useNavigate();
 
   const [formData, setFormData] = useState({
-    email:"", password:""
-  })
+    email: "",
+    password: "",
+  });
 
   useEffect(() => {
-      LoginScript();
+    LoginScript();
   }, []);
 
-  function handleChange(event){
-    setFormData((prevFormData)=>{
-      return{
+  function handleChange(event) {
+    setFormData((prevFormData) => {
+      return {
         ...prevFormData,
-        [event.target.name]:event.target.value
-      }
-    })
+        [event.target.name]: event.target.value,
+      };
+    });
   }
-  async function handleSubmit(e){
-    e.preventDefault()
+  async function handleSubmit(e) {
+    e.preventDefault();
     try {
-      const {data, error} = await supabase.auth.signInWithPassword({
-        email:formData.email,
-        password:formData.password,
-      })
+      const { data, error } = await supabase.auth.signInWithPassword({
+        email: formData.email,
+        password: formData.password,
+      });
 
-      if(error) throw error
-      console.log(data)
-      setToken(data)
-      navigate("/dashboard")
+      if (error) throw error;
+
+      setToken(data);
+      navigate("/dashboard");
       // alert("Check your email for verification link")
     } catch (error) {
-      alert(error)
+      alert(error);
     }
   }
 
   return (
     <>
-    <div className="Title">
-      <h1><Link to={"/"}>Trash Hunter</Link></h1>
-    </div>
-    <div className="SpaceLayout"></div>
-    <div className="Layout0">
-      <img src="./images/img-login.png" className="ImageSmall" alt=""></img>
-      <div className="FormLayout">
-
-        <form className="Form" onSubmit={handleSubmit} method="POST">
-          <fieldset>
-          <legend>Welcome back</legend>
-          <div className="TextSpace">
-            <div className="InputField">
-              <input className="InputText" type="text" name="email" placeholder="email" onChange={handleChange}/>
-            </div>
-          </div>
-          <div className="TextSpace">
-            <div className="PasswordInputField">
-              <input id="inputPassword" className="InputText" type="password" name="password" placeholder="password" onChange={handleChange}/>
-              <img id="togglePassword" src="./images/eye-ic.svg" alt="toggle password" />
-            </div>
-          </div>
-          <div className="InputButtonSpace">
-              <input className="InputButton" type="submit" name="submit" value="LOGIN" />
-          </div>
-          <p className="TextQuestion">Don’t have an account?  <Link to={"/signup"}>Sign up</Link></p>
-          </fieldset>
-        </form>
-
+      <div className="Title">
+        <h1>
+          <Link to={"/"}>Trash Hunter</Link>
+        </h1>
       </div>
-      <img src="./images/img-login.png" className="ImageLarge" alt=""></img>
-    </div>
+      <div className="SpaceLayout"></div>
+      <div className="Layout0">
+        <img src="./images/img-login.png" className="ImageSmall" alt=""></img>
+        <div className="FormLayout">
+          <form className="Form" onSubmit={handleSubmit} method="POST">
+            <fieldset>
+              <legend>Welcome back</legend>
+              <div className="TextSpace">
+                <div className="InputField">
+                  <input className="InputText" type="text" name="email" placeholder="email" onChange={handleChange} />
+                </div>
+              </div>
+              <div className="TextSpace">
+                <div className="PasswordInputField">
+                  <input id="inputPassword" className="InputText" type="password" name="password" placeholder="password" onChange={handleChange} />
+                  <img id="togglePassword" src="./images/eye-ic.svg" alt="toggle password" />
+                </div>
+              </div>
+              <div className="InputButtonSpace">
+                <input className="InputButton" type="submit" name="submit" value="LOGIN" />
+              </div>
+              <p className="TextQuestion">
+                Don’t have an account? <Link to={"/signup"}>Sign up</Link>
+              </p>
+            </fieldset>
+          </form>
+        </div>
+        <img src="./images/img-login.png" className="ImageLarge" alt=""></img>
+      </div>
     </>
   );
 }
 
 function LoginScript() {
-    document.getElementById( "togglePassword" ).onclick = function() {
-        const x = document.getElementById("inputPassword");
-        if (x.type === "password") {
-          x.type = "text";
-        } else {
-          x.type = "password";
-        }
-    };
+  document.getElementById("togglePassword").onclick = function () {
+    const x = document.getElementById("inputPassword");
+    if (x.type === "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  };
 }
 
 export default Login;
--- src/parts/dashboard/EventRegis.jsx
+++ src/parts/dashboard/EventRegis.jsx
@@ -1,12 +1,64 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "../../assets/style/eventregis.css";
 import { useNavigate, useParams } from "react-router-dom";
+import supabase from "../../config/supabaseClient";
+
 function EventRegis() {
   const { id } = useParams();
   const navigate = useNavigate();
+
+  const [field, setField] = useState({
+    created_at: new Date().toISOString(),
+    id: 14,
+    "id-event": parseInt(id),
+    "id-akun": "",
+    name: "",
+    email: "",
+    domicile: "",
+    notelp: "",
+  });
   const handleBack = (id) => {
     navigate(`/dashboard/event/${id}`);
   };
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setField((prevState) => ({
+      ...prevState,
+      [name]: value,
+    }));
+    console.log(field);
+  };
+
+  useEffect(() => {
+    if (sessionStorage.getItem("token")) {
+      let data = JSON.parse(sessionStorage.getItem("token"));
+      setField((prevState) => ({
+        ...prevState,
+        "id-akun": data.user.id,
+      }));
+    }
+  }, []);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    console.log(field);
+
+    const { data } = await supabase.from("daftar").select();
+    console.log(data);
+    try {
+      const { error } = await supabase.from("daftar").insert(field);
+
+      if (error) {
+        alert("Terjadi kesalahan, silahkan coba lagi");
+      } else {
+        alert("Berhasil mendaftar");
+        navigate(`/dashboard/event/${id}`);
+      }
+    } catch (error) {
+      alert(error);
+    }
+  };
+
   return (
     <>
       <div className="Container">
@@ -19,30 +71,30 @@ function EventRegis() {
               <div>Rise Togheter Campaign Volunter</div>
             </div>
             <div>
-              <form className="FormControl">
+              <form className="FormControl" onSubmit={handleSubmit}>
                 <div className="form-group">
-                  <label for="exampleFormControlInput1">Full Name</label>
-                  <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="Full Name" />
+                  <label htmlFor="exampleFormControlInput1">Full Name</label>
+                  <input type="text" className="form-control" id="exampleFormControlInput1" name="name" value={field.name || ""} placeholder="Full Name" onChange={handleChange} />
                 </div>
                 <div className="form-group">
-                  <label for="exampleFormControlInput1">Email</label>
-                  <input type="email" className="form-control" id="exampleFormControlInput1" placeholder="Email" />
+                  <label htmlFor="exampleFormControlInput1">Email</label>
+                  <input type="email" className="form-control" id="exampleFormControlInput1" name="email" value={field.email || ""} placeholder="Email" onChange={handleChange} />
                 </div>
                 <div className="form-group">
-                  <label for="exampleFormControlInput1">No. Telepon</label>
-                  <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="No. Telepon" />
+                  <label htmlFor="exampleFormControlInput1">No. Telepon</label>
+                  <input type="text" className="form-control" id="exampleFormControlInput1" name="notelp" value={field.notelp || ""} placeholder="No. Telepon" onChange={handleChange} />
                 </div>
                 <div className="form-group">
-                  <label for="exampleFormControlInput1">Domisili</label>
-                  <input type="text" className="form-control" id="exampleFormControlInput1" placeholder="Domisili" />
+                  <label htmlFor="exampleFormControlInput1">Domisili</label>
+                  <input type="text" className="form-control" id="exampleFormControlInput1" name="domicile" value={field.domicile || ""} placeholder="Domisili" onChange={handleChange} />
                 </div>
                 <div className="form-group">
-                  <label for="exampleFormControlInput1">Screenshot bukti share poster</label>
-                  <input type="file" className="form-control" id="exampleFormControlInput1" />
+                  <label htmlFor="exampleFormControlInput1">Screenshot bukti share poster</label>
+                  <input type="file" className="form-control" id="exampleFormControlInput1" name="poster" />
                 </div>
                 <div className="form-group">
-                  <label for="exampleFormControlInput1">Screenshot bukti install Aplikasi</label>
-                  <input type="file" className="form-control" id="exampleFormControlInput1" />
+                  <label htmlFor="exampleFormControlInput1">Screenshot bukti install Aplikasi</label>
+                  <input type="file" className="form-control" id="exampleFormControlInput1" name="aplikasi" />
                 </div>
                 <div className="ButtonSubmit">
                   <input type="submit" value="DAFTAR" />
