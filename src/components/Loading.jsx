71f4a903617b5bf2fea58cbda3a69d0e79e4c8ce
yoghaf <utomoyogafirmansyah@gmail.com>
2023-06-13 00:19:51 +0700

    Update Loading.jsx

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -3,9 +3,11 @@ import { Spinner } from "react-bootstrap";
 
 function Loading() {
   return (
-    <div className="d-flex justify-content-center align-items-center">
+    <div className="d-flex justify-content-center align-items-center" style={{ width: "100vw", height: "50vh" }}>
       <Spinner animation="border" role="status">
-        <span className="sr-only">Loading...</span>
+        <span className="sr-only" style={{ fontSize: "24px" }}>
+          Loading...
+        </span>
       </Spinner>
     </div>
   );
